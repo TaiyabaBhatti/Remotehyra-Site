b6c04dd55d1f92e4f0e43f0a20e2ebdcd623dbb4
TaiyabaBhatti <taiyababhattii@gmail.com>
2025-05-07 19:21:52 +0500

    setup inputs and respective design

--- src/pages/Contact/InputBlock.jsx
+++ src/pages/Contact/InputBlock.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function InputBlock({labeltext,inputType}) {
   return (
-    <div className="space-y-2">
-    <label className="text-sm font-medium text-gray-700 bg-white">{labeltext}</label>
-    <input required="" className="w-full p-3 focus:border-b-2  bg-white  border border-gray-200 outline-none transition-all"  type={inputType} value=""/></div>
+    <div tabIndex={0}  className="px-6 pt-3 pb-2 group space-y-2 relative 
+    before:absolute before:left-0 before:bottom-0  before:w-full before:h-0 before:z-50  focus-within:before:h-1 focus-within:pt-0.5 focus-within:before:duration-300 focus-within:duration-300  before:bg-linear-to-r before:from-[#BC01FF] before:to-[#46178f] border border-gray-200 focus:border-gray-400">
+    <label className=" text-lg group-focus-within:text-sm font-medium text-gray-400 bg-white transition-all">{labeltext}</label>
+    <input required="" className="relative bg-white group-focus-within:block w-full hidden   outline-none transition-all"  type={inputType} /></div>
   )
 }
