b7edc4c39977b4bcfc72440e2c7019483ed6fcec
TaiyabaBhatti <taiyababhattii@gmail.com>
2025-05-12 00:27:52 +0500

    added

--- src/pages/Home/ContentLayout.jsx
+++ src/pages/Home/ContentLayout.jsx
@@ -5,7 +5,7 @@ export default function ContentLayout({title,subtitle,btn_text=null,btnDecor,cla
   return (
     <div className={`max-w-[525px] max-desktop-s4:max-w-full  ${classTitle} flex flex-col items-start  gap-y-6 justify-center `}>
              {element}
-              <h1 className={`sub-title text-5xl font-bold`}>
+              <h1 className={`heading text-5xl font-bold`}>
                 {title}
               </h1>
               <p className={`desc font-semibold text-lg`}>
